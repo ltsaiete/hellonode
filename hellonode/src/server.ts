8b66f4a4efa005457207a5c8311a7038884ef594
Luis Saiete <luistsaiete@gmail.com>
2023-06-11 15:36:15 +0200

    Test code with promises

--- hellonode/src/server.ts
+++ hellonode/src/server.ts
@@ -2,8 +2,52 @@ import fastify from 'fastify';
 
 const app = fastify({ logger: true });
 
+const users = [
+	{
+		id: 1,
+		name: 'Lewis',
+		email: 'ltsaiete@gmail.com'
+	}
+];
+
+function listUsers() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Fetching users');
+			resolve(users);
+		}, 2000);
+	});
+}
+
+function addUser(user) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Adding user');
+			user.id = users.length + 1;
+			users.push(user);
+			resolve(users[users.length - 1]);
+		}, 2000);
+	});
+}
+
 app.get('/', (request, reply) => {
 	reply.send({ message: 'Hello world' });
+});
+
+app.get('/users', async (request, reply) => {
+	console.log('Before fetch');
+	const users = await listUsers();
+	console.log('After fetch');
+	reply.send(users);
+});
+
+app.post('/users', async (request, reply) => {
+	const { name, email } = request.body;
+	console.log('Before add');
+	const user = await addUser({ name, email });
+	console.log('after add');
+
+	reply.status(201).send(user);
 });
 
 app
